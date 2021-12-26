f2fabb2120555ee9a7fc8239cdfb3eb232c9bd94
firdaws Aboulaye <aboufred@gmail.com>
2021-12-26 22:56:43 +0100

    Inbox smart contract

---
+++ inbox/deploy.js
@@ -0,0 +1,31 @@
+const env = require("dotenv");
+const HDWalletPrdvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+env.config();
+
+const INITIAL_MESSAGE = "Hi there !";
+const provider = new HDWalletPrdvider(
+  process.env.MNEMONICS,
+  process.env.RINKEBY_API
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: [INITIAL_MESSAGE],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+
+deploy();
---
+++ inbox/test/inbox.test.js
@@ -0,0 +1,40 @@
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const { interface, bytecode } = require("../compile");
+
+const web3 = new Web3(ganache.provider());
+const INITIAL_MESSAGE = "Hi there !";
+
+let accounts;
+let inbox;
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: [INITIAL_MESSAGE],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Inbox", () => {
+  it("Deploy a contract", () => {
+    assert.ok(inbox.options.address);
+  });
+
+  it("Has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_MESSAGE);
+  });
+
+  it("Can change the message", async () => {
+    const newMessage = "Bye";
+    const txId = await inbox.methods
+      .setMessage(newMessage)
+      .send({ from: accounts[0], gas: "1000000" });
+    console.log("TxId:", txId);
+    const message = await inbox.methods.message().call();
+    assert.equal(message, newMessage);
+  });
+});
